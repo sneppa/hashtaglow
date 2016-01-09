9bef0d88e90501f28a60f47eb554aa2bb6955418
Frozen <Frozen@Frozen-PC.fritz.box>
2016-01-09 13:36:30 +0100

    Changed: server.js, added appendToFile-function, Task 5.2, Sebastian Changed: form.txt, used for testing the function (unimportant anyway)

--- Meilenstein 5/js/server.js
+++ Meilenstein 5/js/server.js
@@ -1,27 +1,40 @@
-// import HTTP module, url, and file system
+//import HTTP module, url, and file system
 var http = require('http');
 var url = require('url');
 var fs = require("fs");
 
-// Server Config
+//Server Config
 const PORT=8000;
 const IP="127.0.0.1";
 
-// create server variable
+//create server variable
 var server = http.createServer(function (request, response) {
-	var queryData = url.parse(request.url, true).query;
-	var myString = queryData.vorname + ' ' + queryData.name + ', ' + queryData.hcoach + ', ' + queryData.acoach + ', ' + queryData.position + ', ' + queryData.number + '\n';
 	
-	// TODO - didn't write own parse
-	fs.appendFile('../db/form.txt', myString, function (err) {
+	//TODO filtering the "favicon-request", better select the form-request and drop everything else!
+	if(request.url.indexOf('ico') > -1){
+		console.log('requested ico')
+		return;	
+	}
+	console.log('called')
+	var queryData = url.parse(request.url, true).query;
+	var myString = queryData.vorname + ' ' + queryData.name + ', ' + queryData.jahr + ', ' + queryData.hcoach + ', ' + queryData.acoach + ', ' + queryData.position + ', ' + queryData.number + '\n';
 
-		// show console log and end response with message
-		console.log('<User connected to Server>');
-		response.end('Sie haben sich erfolgreich auf den WebServer mit der Url ' + IP + ':' + PORT + ' verbunden');
-	});
+	appendToFile('../db/form.txt', myString);
+	response.end('Sie haben sich erfolgreich auf den WebServer mit der Url ' + IP + ':' + PORT + ' verbunden');
+	
 });
 
-// start the Server
+//start the Server
 server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+	console.log("Server listening on: http://localhost:%s", PORT);
 });
+
+function appendToFile(path, appendingString){
+	fs.open(path, 'a', function( err, fd ) {
+		fs.write( fd, appendingString, null, 'utf8', function(){
+			fs.close(fd, function(){
+				console.log('file closed')
+			});
+		});
+	});
+}
